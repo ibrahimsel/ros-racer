8ea9de5d121dd1d6f18285dcbaf5d9117f6812df
Ibrahim Sel <ibrahim.sel@eteration.com>
2024-11-15 10:45:22 +0300

    update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ RUN apt update && apt install -y \
     python3-colcon-common-extensions \
     python3-rosdep \
     python3-pip \
+    python3-vcstool \
     ros-humble-ackermann-msgs \
     ros-humble-tf2-msgs \
     git
@@ -14,14 +15,14 @@ COPY . /root/muto/
 
 WORKDIR /root/muto
 
-RUN mkdir launch/ config/
+RUN mkdir launch/ config/ src/
+RUN vcs import src < muto.repos
 COPY ./muto.launch.py /root/muto/launch/
 COPY ./muto.yaml /root/muto/config/
 
-RUN ./setup_ws.bash
-RUN rosdep install --from-paths src --ignore-src -r -y
+RUN rosdep install --from-paths . --ignore-src -r -y
 RUN . /opt/ros/humble/setup.sh && colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
 
 RUN echo "source /root/muto/install/setup.bash" >> ~/.bashrc
 
-ENTRYPOINT ["/bin/bash", "-c", "ls && pwd && source ./install/setup.bash && ros2 launch ./launch/muto.launch.py"]
+ENTRYPOINT ["/bin/bash", "-c", "source ./install/setup.bash && ros2 launch ./launch/muto.launch.py"]
--- muto.launch.py
+++ muto.launch.py
@@ -2,104 +2,140 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
+import socket
+import uuid
 
 
 def generate_launch_description():
-    muto_namespace_arg = DeclareLaunchArgument(
-        "muto_namespace",
-        default_value="muto"
+    hostname = socket.gethostname()
+    # Arguments
+    muto_namespace_arg = DeclareLaunchArgument("muto_namespace", default_value="muto")
+    vehicle_id_namespace_arg = DeclareLaunchArgument(
+        "vehicle_id_namespace",
+        default_value="org.eclipse.muto.sandbox",
+        description="Vehicle ID namespace",
     )
+    vehicle_id_arg = DeclareLaunchArgument(
+        "vehicle_id", default_value=f"{hostname}", description="Edge Device ID"
+    )
+
+    # Files
     muto_params = "./config/muto.yaml"
 
+    # Agent
     node_agent = Node(
         namespace=LaunchConfiguration("muto_namespace"),
-        name="agent",
+        name=f"agent_{hostname}",
         package="agent",
         executable="muto_agent",
         output="screen",
         parameters=[
-            muto_params            
-        ]
+            muto_params,
+            {"namespace": LaunchConfiguration("vehicle_id_namespace")},
+            {"name": LaunchConfiguration("vehicle_id")},
+        ],
     )
 
     node_mqtt_gateway = Node(
         namespace=LaunchConfiguration("muto_namespace"),
-        name="gateway",
+        name=f"gateway_{hostname}",
         package="agent",
         executable="mqtt",
         output="screen",
         parameters=[
-            muto_params            
-        ]
+            muto_params,
+            {"namespace": LaunchConfiguration("vehicle_id_namespace")},
+            {"name": LaunchConfiguration("vehicle_id")},
+        ],
     )
 
     node_commands = Node(
         namespace=LaunchConfiguration("muto_namespace"),
-        name="commands_plugin",
+        name=f"commands_plugin_{hostname}",
         package="agent",
         executable="commands",
         output="screen",
         parameters=[
-            muto_params            
-        ]
+            muto_params,
+            {"namespace": LaunchConfiguration("vehicle_id_namespace")},
+            {"name": LaunchConfiguration("vehicle_id")},
+        ],
     )
 
+    # Core
     node_twin = Node(
         namespace=LaunchConfiguration("muto_namespace"),
-        name="core_twin",
+        name=f"core_twin_{hostname}",
         package="core",
         executable="twin",
         output="screen",
         parameters=[
-            muto_params            
-        ]
+            muto_params,
+            {"namespace": LaunchConfiguration("vehicle_id_namespace")},
+            {"name": LaunchConfiguration("vehicle_id")},
+        ],
     )
 
+    # Composer
     node_composer = Node(
+        name=f"composer_{hostname}",
         namespace=LaunchConfiguration("muto_namespace"),
         package="composer",
         executable="muto_composer",
         output="screen",
         parameters=[
-            muto_params            
-        ]
+            muto_params,
+            {"namespace": LaunchConfiguration("vehicle_id_namespace")},
+            {"name": LaunchConfiguration("vehicle_id")},
+        ],
     )
 
     node_compose_plugin = Node(
+        name=f"compose_plugin_{hostname}",
         namespace=LaunchConfiguration("muto_namespace"),
         package="composer",
         executable="compose_plugin",
         output="screen",
         parameters=[
-            muto_params            
-        ]
+            muto_params,
+            {"namespace": LaunchConfiguration("vehicle_id_namespace")},
+            {"name": LaunchConfiguration("vehicle_id")},
+        ],
     )
 
     node_native_plugin = Node(
+        name=f"native_plugin_{hostname}",
         namespace=LaunchConfiguration("muto_namespace"),
         package="composer",
         executable="native_plugin",
         output="screen",
         parameters=[
-            muto_params            
-        ]
+            muto_params,
+            {"namespace": LaunchConfiguration("vehicle_id_namespace")},
+            {"name": LaunchConfiguration("vehicle_id")},
+        ],
     )
 
     node_launch_plugin = Node(
+        name=f"launch_plugin_{hostname}",
         namespace=LaunchConfiguration("muto_namespace"),
         package="composer",
         executable="launch_plugin",
         output="screen",
         parameters=[
-            muto_params            
-        ]
+            muto_params,
+            {"namespace": LaunchConfiguration("vehicle_id_namespace")},
+            {"name": LaunchConfiguration("vehicle_id")},
+        ],
     )
 
-
+    # Launch Description Object
     ld = LaunchDescription()
 
     ld.add_action(muto_namespace_arg)
-    
+    ld.add_action(vehicle_id_namespace_arg)
+    ld.add_action(vehicle_id_arg)
+
     ld.add_action(node_agent)
     ld.add_action(node_mqtt_gateway)
     ld.add_action(node_commands)
